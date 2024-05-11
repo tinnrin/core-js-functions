054a1563f6a2fb19b38a7e7d8d508c5e5eee6170
tinnrin <tinnrin@gmail.com>
2024-05-11 22:07:38 +0300

    feat: add several solutions

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -201,8 +201,10 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function f(...args2) {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
@@ -222,8 +224,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom;
+  return function f() {
+    const res = id;
+    id += 1;
+    return res;
+  };
 }
 
 module.exports = {
